250992466bb8cdca01a4510f76bdac7f64396bbb
DanDiplo <dan@diplo.co.uk>
2018-07-05 21:39:18 +0100

    Simplified the code for checking internal links only and limiting codes to specific statuses

--- Diplo.LinkChecker/Controllers/LinkCheckerController.cs
+++ Diplo.LinkChecker/Controllers/LinkCheckerController.cs
@@ -14,8 +14,6 @@ using Umbraco.Web.WebApi;
 
 namespace Diplo.LinkChecker.Controllers
 {
-    using Lucene.Net.Search;
-
     /// <summary>
     /// Controller for checking links and related tasks
     /// </summary>
@@ -29,14 +27,13 @@ namespace Diplo.LinkChecker.Controllers
         /// <param name="timeout">The timeout period (in seconds) before the checker abandons trying to connect to a server</param>
         /// <param name="omitPortDuringChecks">Omits the port number when checking a page</param>
         /// <param name="checkInternalLinksOnly">Skip checking external links</param>
-        /// <param name="onlyShowErrors">Display only errors, not 200s</param>
-        /// <param name="showErrors">Error code types to display (default = 300,400,500)</param>
+        /// <param name="showErrors">Error code types to display</param>
         /// <returns>A list of checked links</returns>
         /// <remarks>
         /// /Umbraco/Backoffice/Api/LinkChecker/CheckPage/1073
         /// </remarks>
         [HttpGet]
-        public async Task<CheckedPage> CheckPage(int id, bool checkEntireDocument = false, int timeout = 30, bool omitPortDuringChecks = false, bool checkInternalLinksOnly = false, bool onlyShowErrors = false, string showErrors = "300,400,500")
+        public async Task<CheckedPage> CheckPage(int id, bool checkEntireDocument = false, int timeout = 30, bool omitPortDuringChecks = false, bool checkInternalLinksOnly = false, string showErrors = null)
         {
             var node = Umbraco.TypedContent(id);
 
@@ -45,122 +42,33 @@ namespace Diplo.LinkChecker.Controllers
                 throw new ArgumentOutOfRangeException("No node could be found with an id of " + id);
             }
 
-            if (timeout < 1)
-            {
-                timeout = 1;
-            }
+            timeout = timeout < 1 ? 1 : timeout;
 
             CheckedPage page = new CheckedPage(node);
 
-            HttpCheckerService checker = new HttpCheckerService();
-            checker.Timeout = TimeSpan.FromSeconds(timeout);
+            HttpCheckerService checker = new HttpCheckerService
+            {
+                Timeout = TimeSpan.FromSeconds(timeout)
+            };
 
             string html = await checker.GetHtmlFromUrl(new Uri(node.UrlAbsolute()));
 
-            HtmlParsingService parser = new HtmlParsingService(new Uri(Request.RequestUri.GetLeftPart(UriPartial.Authority)), !omitPortDuringChecks);
-            parser.CheckEntireDocument = checkEntireDocument;
+            HtmlParsingService parser = new HtmlParsingService(new Uri(Request.RequestUri.GetLeftPart(UriPartial.Authority)), !omitPortDuringChecks)
+            {
+                CheckEntireDocument = checkEntireDocument
+            };
 
             var links = parser.GetLinksFromHtmlDocument(html);
 
             if (links != null && links.Any())
             {
-                if (checkInternalLinksOnly)
-                {
-                    var internalLinks = links.Where(n => n.IsInternal);
-                    page.CheckedLinks = await checker.CheckLinks(internalLinks);
-                }
-                else
-                {
-                    page.CheckedLinks = await checker.CheckLinks(links);
-                }
-                
+                page.CheckedLinks = checkInternalLinksOnly ? await checker.CheckLinks(links.Where(n => n.IsInternal)) : await checker.CheckLinks(links);
             }
 
-            //Set Display based on error type & options
-            var displayAllErrors = showErrors ==  "300,400,500";
-            var displayErrorCodes = showErrors.Split(',').ToList();
-            var displayPrefixes = new List<string>();
-            if (!onlyShowErrors)
+            if (!String.IsNullOrEmpty(showErrors))
             {
-                displayErrorCodes.Add("200");
-            }
-
-            foreach (var code in displayErrorCodes)
-            {
-                if (code.Length > 0)
-                {
-                    var prefix = code.Substring(0, 1);
-                    displayPrefixes.Add(prefix);
-                }
-            }
-
-            if (page.CheckedLinks!=null && page.CheckedLinks.Any())
-            {
-                var updatedLinks = new List<Link>();
-
-                //var linksWithCodes = page.CheckedLinks.Where(n => !string.IsNullOrEmpty(n.StatusCode)).ToList();
-                //var linksWithOutCodes = page.CheckedLinks.Where(n => string.IsNullOrEmpty(n.StatusCode)).ToList();
-
-                foreach (var checkedLink in page.CheckedLinks.ToList())
-                {
-                    //Link with status code
-                    if (!string.IsNullOrEmpty(checkedLink.StatusCode))
-                    {
-                        var statusCodePrefix = checkedLink.StatusCode.Substring(0, 1);
-                        if (displayPrefixes.Contains(statusCodePrefix))
-                        {
-                            checkedLink.IsDisplayCode = true;
-                        }
-                        else
-                        {
-                            checkedLink.IsDisplayCode = false;
-                        }
-                    }
-                    else //Link without status code
-                    {
-                        if (displayAllErrors)
-                        {
-                            //Also show errors without a code
-                            checkedLink.IsDisplayCode = true;
-                        }
-                        else
-                        {
-                            checkedLink.IsDisplayCode = false;
-                        }
-                    }
-
-                    updatedLinks.Add(checkedLink);
-                }
-
-
-                //foreach (var code in displayErrorCodes)
-                //{
-                //    if (code.Length > 0)
-                //    {
-                //        var prefix = code.Substring(0, 1);
-                //        var matches = linksWithCodes.Where(n => n.StatusCode.StartsWith(prefix)).ToList();
-                //        if (matches.Any())
-                //        {
-                //            foreach (var match in matches)
-                //            {
-                //                match.IsDisplayCode = true;
-                //                updatedLinks.Add(match);
-                //            }
-                //        }
-                //    }
-                //}
-
-                //if (displayAllErrors)
-                //{
-                //    //Also show errors without a code
-                //    foreach (var match in linksWithOutCodes)
-                //    {
-                //        match.IsDisplayCode = true;
-                //        updatedLinks.Add(match);
-                //    }
-                //}
-
-                page.CheckedLinks = updatedLinks;
+                var errorStatusCodes = showErrors.Split(',').Select(e => e.Trim());
+                page.CheckedLinks = page.CheckedLinks.Where(l => l.IsSuccessCode || errorStatusCodes.Contains(l.StatusCode)); // filter results by status code
             }
 
             return page;
