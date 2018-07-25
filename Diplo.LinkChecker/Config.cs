80b4aa80cdbb333bb45cba0b6bfadc92a77cd4c2
DanDiplo <dan@diplo.co.uk>
2018-07-25 11:06:20 +0100

    Commented what config does

--- Diplo.LinkChecker/Config.cs
+++ Diplo.LinkChecker/Config.cs
@@ -6,6 +6,10 @@ namespace Diplo.LinkChecker
 {
     internal static class Config
     {
+        /// <summary>
+        /// Gets an instance of the client handler that has configuration values for the HTTP services
+        /// </summary>
+        /// <returns>An HTTP client handler</returns>
         public static HttpClientHandler GetClientHandler()
         {
             return new HttpClientHandler()
